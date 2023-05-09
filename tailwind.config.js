96c5076f081369a329203fef449464154b78e3db
NiengeDavid <97910242+NiengeDavid@users.noreply.github.com>
2023-05-09 23:46:24 +0100

    Committed via upload by Nienge David
    Made corrections to about page as reviewed, coded the centres page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,10 @@ module.exports = {
       '3xl': '1.953rem',
       '4xl': '2.441rem',
       '5xl': '3.052rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
     }
   },
   plugins: [
